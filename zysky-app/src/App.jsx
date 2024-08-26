049cb2b92a464855c566bbd92d1c553c2eb0d467
KeshalHetti <kaneeshakeshal@gmail.com>
2024-08-26 18:47:38 +1000

    added sign up page

--- zysky-app/src/App.jsx
+++ zysky-app/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css'
 import Login from './Components/login'
+import Signup from './Components/signup'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-       <Login />
+       {/* <Login /> */}
+       <Signup />
+
     </div>
   )
 }
