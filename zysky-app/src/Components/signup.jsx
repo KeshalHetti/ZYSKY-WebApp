eb5e669818b93ae988aee39a696b4055a03b54c1
Hetti <hetti@dynalli.com>
2024-08-26 22:29:25 +0530

    Signup page

--- zysky-app/src/Components/signup.jsx
+++ zysky-app/src/Components/signup.jsx
@@ -3,91 +3,101 @@ import "../App.css"; // Import the CSS for styling
 import logo from '../assets/img/zysky-splash-small 1@2x.png';
 
 const SignUp = () => {
-  return (
-    <div>
-      {/* Logo */}
-      <div className="logo">
-        <img src={logo} alt="Logo" />
-        <h2>Partner Registration</h2>
-      </div>
-
-        <form>
-            <fieldset>
-            <legend>New Partner Account</legend>
-                <div className="form-row">
-                <div className="Signup-form">
-                    <input type="text" className="form-control" placeholder="Firstname" required />
+    return (
+        <div>
+            <div class="row">
+                <div class="col-3">
+                    &nbsp;
                 </div>
-                <div className="Signup-form">
-                    <input type="text" className="form-control" placeholder="Company Name" required />
-                </div>
-                </div>
-
-                <div className="form-row ">
-                <div className="Signup-form">
-                    <input type="text" className="form-control" placeholder="Lastname" required />
-                </div>
-                <div className="form-row ">
-                <div className="Signup-form">
-                    <input type="text" className="form-control" placeholder="ZIP" required />
-                    <input type="text" className="form-control" placeholder="City" required />
-                </div>
-                </div>
-                </div>
-
-                <div className="form-row">
-                <div className="Signup-form">
-                    <input type="email" className="form-control" placeholder="Your Email" required />
-                </div>
-                <div className="Signup-form">
-                    <input type="text" className="form-control" placeholder="Street & Number" required />
-                </div>
-                </div>
-
-                <div className="form-row">
-                <div className="Signup-form">
-                    <input type="email" className="form-control" placeholder="Confirm Your Email" required />
-                </div>
-                <div className="Signup-form">
-                    <input type="text" className="form-control" placeholder="Phone" required />
-                </div>
-                </div>
-
-                <div className="form-row">
-                <div className="Signup-form">
-                    <input type="password" className="form-control" placeholder="Password" required />
-                </div>
-                <div className="Signup-form">
-                    <input type="text" className="form-control" placeholder="NIP" required />
-                </div>
-                </div>
-
-                <div className="form-row">
-                <div className="Signup-form">
-                    <input type="password" className="form-control" placeholder="Confirm Password" required />
-                </div>
-                <div className="Signup-form">
-                    <p>You must read and accept the following documents to continue</p>
-                    <div className="checkbox-group">
-                    <label>
-                        <input type="checkbox" required /> Privacy Policy
-                    </label>
-                    <label>
-                        <input type="checkbox" required /> Terms of Service
-                    </label>
+                <div class="col-6">
+                    <div className="logo">
+                        <img src={logo} alt="Logo" />
                     </div>
                 </div>
+                <div class="col-3">
+                    &nbsp;
                 </div>
+            </div>
 
-                <div className="form-row">
-                <button type="submit" className="btn btn-primary">Create Account</button>
+            <h2 className='p-b-30'>Partner Registration</h2>
+            <form>
+                <div className='fieldset'>
+                    <div className='legend'>New Partner Account</div>
+                    <div className="row">
+                        <div className="col-lg-6 col-md-12">
+                            <div className="form-floating">
+                                <input type="text" className="form-control" id="Firstname" placeholder="Firstname" />
+                                <label for="Firstname">Firstname</label>
+                            </div>
+                            <div className="form-floating m-b-20">
+                                <input type="text" className="form-control" id="Lastname" placeholder="Lastname" />
+                                <label for="Lastname">Lastname</label>
+                            </div>
+                            <div className="form-floating">
+                                <input type="email" className="form-control" id="Youremail" placeholder="Your Email" />
+                                <label for="Youremail">Your Email</label>
+                            </div>
+                            <div className="form-floating m-b-20">
+                                <input type="email" className="form-control" id="Confirmemail" placeholder="Confirm Your Email" />
+                                <label for="Confirmemail">Confirm Your Email</label>
+                            </div>
+                            <div className="form-floating">
+                                <input type="password" className="form-control" id="Password" placeholder="Password" />
+                                <label for="Password">Password</label>
+                            </div>
+                            <div className="form-floating">
+                                <input type="password" className="form-control" id="Confirmpassword" placeholder="Confirm Password" />
+                                <label for="Confirmpassword">Confirm Password</label>
+                            </div>
+
+                        </div>
+                        <div className="col-lg-6 col-md-12">
+                            <div className="form-floating">
+                                <input type="text" className="form-control" id="Companyname" placeholder="Company Name" />
+                                <label for="Companyname">Company Name</label>
+                            </div>
+                            <div className='row'>
+                                <div className='col'>
+                                    <div class="form-floating">
+                                        <input type="text" className="form-control" id="Zip" placeholder="Zip" />
+                                        <label for="Zip">Zip</label>
+                                    </div>
+                                </div>
+                                <div className='col'>
+                                    <div className="form-floating">
+                                        <input type="text" className="form-control" id="City" placeholder="City" />
+                                        <label for="City">City</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="form-floating">
+                                <input type="text" className="form-control" id="Streetnumber" placeholder="Street & Number" />
+                                <label for="Streetnumber">Street & Number</label>
+                            </div>
+                            <div className="form-floating">
+                                <input type="text" className="form-control" id="Phone" placeholder="Phone" />
+                                <label for="Phone">Phone</label>
+                            </div>
+                            <div className="form-floating">
+                                <input type="text" className="form-control" id="NIP" placeholder="NIP" />
+                                <label for="NIP">NIP</label>
+                            </div>
+                            <span>You must read and accept the following documents to continue</span>
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                <label className="form-check-label" for="flexCheckDefault">
+                                    Default checkbox
+                                </label>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </fieldset>
-        </form>
-
-      
-    </div>
-  );
+                <div className="float-right p-t-30">
+              <button type="button" className="btn btn-success m-l-15">Utwórz konto</button>
+            </div>
+            </form>
+        </div>
+    );
 };
 
 export default SignUp;
