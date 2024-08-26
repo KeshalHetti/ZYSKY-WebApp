f8ea34b747e3a51c26b50c5df0418c64424d2d11
KeshalHetti <kaneeshakeshal@gmail.com>
2024-08-26 18:47:00 +1000

    created the signup page

---
+++ zysky-app/src/Components/signup.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import "../App.css"; // Import the CSS for styling
+import logo from '../assets/img/zysky-splash-small 1@2x.png';
+
+const PartnerRegistration = () => {
+  return (
+    <div>
+      {/* Logo */}
+      <div className="logo">
+        <img src={logo} alt="Logo" />
+        <h2>Partner Registration</h2>
+      </div>
+
+        <form className="registration-form">
+            <fieldset>
+            <legend>Personalia:</legend>
+                <div className="form-row">
+                <div className="form-group">
+                    <input type="text" className="form-control" placeholder="Firstname" required />
+                </div>
+                <div className="form-group">
+                    <input type="text" className="form-control" placeholder="Company Name" required />
+                </div>
+                </div>
+
+                <div className="form-row">
+                <div className="form-group">
+                    <input type="text" className="form-control" placeholder="Lastname" required />
+                </div>
+                <div className="form-group">
+                    <input type="text" className="form-control" placeholder="ZIP" required />
+                    <input type="text" className="form-control" placeholder="City" required />
+                </div>
+                </div>
+
+                <div className="form-row">
+                <div className="form-group">
+                    <input type="email" className="form-control" placeholder="Your Email" required />
+                </div>
+                <div className="form-group">
+                    <input type="text" className="form-control" placeholder="Street & Number" required />
+                </div>
+                </div>
+
+                <div className="form-row">
+                <div className="form-group">
+                    <input type="email" className="form-control" placeholder="Confirm Your Email" required />
+                </div>
+                <div className="form-group">
+                    <input type="text" className="form-control" placeholder="Phone" required />
+                </div>
+                </div>
+
+                <div className="form-row">
+                <div className="form-group">
+                    <input type="password" className="form-control" placeholder="Password" required />
+                </div>
+                <div className="form-group">
+                    <input type="text" className="form-control" placeholder="NIP" required />
+                </div>
+                </div>
+
+                <div className="form-row">
+                <div className="form-group">
+                    <input type="password" className="form-control" placeholder="Confirm Password" required />
+                </div>
+                <div className="form-group">
+                    <p>You must read and accept the following documents to continue</p>
+                    <div className="checkbox-group">
+                    <label>
+                        <input type="checkbox" required /> Privacy Policy
+                    </label>
+                    <label>
+                        <input type="checkbox" required /> Terms of Service
+                    </label>
+                    </div>
+                </div>
+                </div>
+
+                <div className="form-row">
+                <button type="submit" className="btn btn-primary">Create Account</button>
+                </div>
+            </fieldset>
+        </form>
+
+      
+    </div>
+  );
+};
+
+export default PartnerRegistration;
