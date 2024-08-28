8c148331457fd8b38a31f62e325ac3e9b6ee8b89
KeshalHetti <kaneeshakeshal@gmail.com>
2024-08-29 02:03:24 +1000

    Create Home page Container

--- zysky-app/src/pages/Home.jsx
+++ zysky-app/src/pages/Home.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+    
+    <div className='homepage-container'>
+    <h1 className='p-b-30'>Welcome to ZYSKY Partner Portal</h1>
+            <form>
+                <div className='fieldset homefeildset'>
+                    <div className='legend'>Statistics</div>
+                </div>
+
+                <div className='fieldset'>
+                    <div className='legend'>Quick Start</div>
+                </div>
+            </form>
+    
+
+    </div>
+    
+  
+  );
   };
   
   export default Home;
