21e9e77b4e671a6ac732c9668bf542cca688a839
Hetti <hetti@dynalli.com>
2024-08-28 20:58:43 +0530

    Side menu

--- zysky-app/src/pages/Layout.jsx
+++ zysky-app/src/pages/Layout.jsx
@@ -12,16 +12,25 @@ const Layout = () => {
     
     <ul id="menu">
     <li>
-            <Link to="/Home">Home</Link>
+            <Link to="/Home"><i className="icon-home"></i><span>Home</span></Link>
           </li>
           <li>
-            <Link to="/login">Login</Link>
+            <Link to="/login"><i className="icon-promotions"></i><span>Promos</span></Link>
           </li>
           <li>
-            <Link to="/signup">Signup</Link>
+            <Link to="/signup"><i className="icon-map"></i><span>Locations</span></Link>
           </li>
           <li>
-            <Link to="/locations">Locations</Link>
+            <Link to="/locations"><i className="icon-bell"></i><span>Notifications</span></Link>
+          </li>
+          <li>
+            <Link to="/locations"><i className="icon-dialog"></i><span>Transactions</span></Link>
+          </li>
+          <li>
+            <Link to="/locations"><i className="icon-account"></i><span>Account</span></Link>
+          </li>
+          <li>
+            <Link to="/locations"><i className="icon-log-out"></i><span>Logout</span></Link>
           </li>
     </ul>
     <div className="content"><Outlet /></div>
