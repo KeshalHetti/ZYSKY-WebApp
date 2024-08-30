a97892dfeceb1956ad060767e8d96ee7b29c9a76
KeshalHetti <kaneeshakeshal@gmail.com>
2024-08-30 19:21:54 +1000

    Created the Transactions page

---
+++ zysky-app/src/pages/Transactions.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import "../App.css"; // Import the CSS for styling
+import { Outlet, Link } from "react-router-dom";
+
+const Transactions = () => {
+    return (
+        <div>
+            <div className='bg-yelloish-green'>
+                <div className="container text-center">
+                    <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2">
+                        <div className="col text-left"><h4>Transactions</h4>
+                            <p>Here is the list of your loyal client’s transactions.  </p>
+                        </div>
+                    </div>
+                </div>
+ 
+            </div>
+            <div className="content-container-no-radious text-center">
+            <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">Date&Time</th>
+                    <th scope="col">Client</th>
+                    <th scope="col">Promo (if used)</th>
+                    <th scope="col">Location</th>
+                    <th scope="col">Amount (if provided)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td>2024-05-22 21:20:38</td>
+                    <td>p****@k*******.pl</td>
+                    <td>Promo 1 Name</td>
+                    <td>Location 1</td>
+                    <td>245</td>
+                    </tr>
+                    <tr>
+                    <td>2024-05-22 20:10:10</td>
+                    <td>d****@g****.com</td>
+                    <td>Promo 2 Name</td>
+                    <td>Location 2</td>
+                    <td>45</td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+           
+        </div>
+
+    );
+};
+
+export default Transactions;
